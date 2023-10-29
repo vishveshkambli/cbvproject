4dd1b9b5736b34f3a7c4c7fb75930ae08a3773ec
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-29 18:45:35 +0530

    models

--- cbvapp/models.py
+++ cbvapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Emp(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.CharField(max_length=30)
+    contact=models.CharField(max_length=30)
+    age=models.IntegerField(max_length=30)
+    gender=models.CharField(max_length=30)
+    addresss=models.TextField(max_length=300)
+    
+    #table name
+    class Meta:
+        db_table='emp'
+        
+from django import forms
+
+class EmpForm(forms.ModelForm):
+    class Meta:
+        Model=Emp
+        field='__all__'
