0a0e4cf6845c2c0f6cf220c8fc97627bee091f57
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-29 18:50:44 +0530

    views

--- cbvapp/views.py
+++ cbvapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import EmpForm #imported for EmpRegister
 
 # Create your views here.
+# from django.views.generic import TemplateView
 
 def homef(request):
     return render(request,'home.html')
+
+class EmpRegister(EmpForm):
+    form_class=EmpForm
+    template_name='addemp.html'
